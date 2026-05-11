cb8eb0534c85c3d81addbb2c46f9efa43653c160
sif5 <sif5@VHAM-9FDAAAE906>
2026-05-11 23:47:51 +1200

    Add project files.

---
+++ ConcreteCalculate - Main/ConcreteCalculate/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace ConcreteCalculate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxLength = new System.Windows.Forms.TextBox();
+            this.textBoxVolume = new System.Windows.Forms.TextBox();
+            this.textBoxCost = new System.Windows.Forms.TextBox();
+            this.textBoxWeight = new System.Windows.Forms.TextBox();
+            this.textBoxBags = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Results = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(75, 172);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 0;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(156, 172);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(140, 23);
+            this.buttonCalculate.TabIndex = 1;
+            this.buttonCalculate.Text = "Calculate Concrete Cost";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(113, 111);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWidth.TabIndex = 3;
+            this.textBoxWidth.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxLength
+            // 
+            this.textBoxLength.Location = new System.Drawing.Point(246, 111);
+            this.textBoxLength.Name = "textBoxLength";
+            this.textBoxLength.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLength.TabIndex = 4;
+            this.textBoxLength.TextChanged += new System.EventHandler(this.textBoxLength_TextChanged);
+            // 
+            // textBoxVolume
+            // 
+            this.textBoxVolume.Location = new System.Drawing.Point(246, 264);
+            this.textBoxVolume.Name = "textBoxVolume";
+            this.textBoxVolume.ReadOnly = true;
+            this.textBoxVolume.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVolume.TabIndex = 5;
+            this.textBoxVolume.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBoxCost
+            // 
+            this.textBoxCost.Location = new System.Drawing.Point(113, 264);
+            this.textBoxCost.Name = "textBoxCost";
+            this.textBoxCost.ReadOnly = true;
+            this.textBoxCost.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCost.TabIndex = 6;
+            // 
+            // textBoxWeight
+            // 
+            this.textBoxWeight.Location = new System.Drawing.Point(113, 312);
+            this.textBoxWeight.Name = "textBoxWeight";
+            this.textBoxWeight.ReadOnly = true;
+            this.textBoxWeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWeight.TabIndex = 8;
+            this.textBoxWeight.TextChanged += new System.EventHandler(this.textBoxWeight_TextChanged);
+            // 
+            // textBoxBags
+            // 
+            this.textBoxBags.Location = new System.Drawing.Point(246, 312);
+            this.textBoxBags.Name = "textBoxBags";
+            this.textBoxBags.ReadOnly = true;
+            this.textBoxBags.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBags.TabIndex = 7;
+            this.textBoxBags.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Width (m)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Length (m)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(149, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Cost";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(143, 296);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Weight (kg)";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(275, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Volume";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(281, 296);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Bags";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(194, 65);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 25);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Input";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Results
+            // 
+            this.Results.AutoSize = true;
+            this.Results.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Results.Location = new System.Drawing.Point(185, 224);
+            this.Results.Name = "Results";
+            this.Results.Size = new System.Drawing.Size(83, 25);
+            this.Results.TabIndex = 17;
+            this.Results.Text = "Results";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(302, 172);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 18;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.Location = new System.Drawing.Point(366, 275);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(78, 50);
+            this.buttonHelp.TabIndex = 19;
+            this.buttonHelp.Text = "Help";
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            this.buttonHelp.Click += new System.EventHandler(this.buttonHelp_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(89, 27);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(298, 25);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Driveway Concrete Calculator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 346);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.Results);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxWeight);
+            this.Controls.Add(this.textBoxBags);
+            this.Controls.Add(this.textBoxCost);
+            this.Controls.Add(this.textBoxVolume);
+            this.Controls.Add(this.textBoxLength);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.buttonClear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxLength;
+        private System.Windows.Forms.TextBox textBoxVolume;
+        private System.Windows.Forms.TextBox textBoxCost;
+        private System.Windows.Forms.TextBox textBoxWeight;
+        private System.Windows.Forms.TextBox textBoxBags;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label Results;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ ConcreteCalculate - Main/ConcreteCalculate/Form1.cs
@@ -0,0 +1,265 @@
+﻿// Skye Faile
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConcreteCalculate
+{
+    public partial class Form1 : Form
+    {
+        //The depth of every driveway.
+        const double DRIVEWAY_DEPTH = 0.5;
+        //Amount of concrete created from 1 kg of cement
+        const double CONCRETE_PER_KG = 1.5;
+        //Weight of a bag of cement
+        const double BAG_WEIGHT = 2.0;
+        //Cost of a bag of cement
+        const decimal BAG_COST = 15.5m;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The method will check that the user enters a valid value, if the value
+        /// isn't valid, a error message will be displayed. It then takes the
+        /// user's values and calculates the volume of concrete required, the amount
+        /// of cement needed, the number of bags needed, and the total cost. It then displays
+        /// these values. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Declare variables
+                double volume = 0;
+                double weight = 0;
+                decimal cost = 0;
+                int bags = 0;
+                double length = 0;
+                double width = 0;
+
+                // Validate the user's input
+                bool lengthValid = double.TryParse(textBoxLength.Text, out length) && length >= 1 && length <= 100;
+                bool widthValid = double.TryParse(textBoxWidth.Text, out width) && width >= 1 && width <= 100;
+
+                // If length and width are valid
+                if (!lengthValid && !widthValid)
+                {
+                    // Display error message
+                    MessageBox.Show("Width and Length must be numbers between 1 and 100.");
+                    // Clear all the textboxes
+                    textBoxWidth.Clear();
+                    textBoxLength.Clear();
+                    textBoxCost.Clear();
+                    textBoxVolume.Clear();
+                    textBoxWeight.Clear();
+                    textBoxBags.Clear();
+                    // Set focus to the first textbox
+                    textBoxWidth.Focus();
+                    // Stop the program from continuing
+                    return;
+                }
+                // If just length is valid
+                else if (!lengthValid)
+                {
+                    // Display error message
+                    MessageBox.Show("Length must be a number between 1 and 100.");
+                    // Clear all the textboxes
+                    textBoxWidth.Clear();
+                    textBoxLength.Clear();
+                    textBoxCost.Clear();
+                    textBoxVolume.Clear();
+                    textBoxWeight.Clear();
+                    textBoxBags.Clear();
+                    // Set focus to the first textbox
+                    textBoxWidth.Focus();
+                    // Stop the program from continuing
+                    return;
+                }
+                // If just width is valid
+                else if (!widthValid)
+                {
+                    // Display error message
+                    MessageBox.Show("Width must be a number between 1 and 100.");
+                    // Clear all the textboxes
+                    textBoxWidth.Clear();
+                    textBoxLength.Clear();
+                    textBoxCost.Clear();
+                    textBoxVolume.Clear();
+                    textBoxWeight.Clear();
+                    textBoxBags.Clear();
+                    // Set focus to the first textbox
+                    textBoxWidth.Focus();
+                    // Stop the program from continuing
+                    return;
+                }
+
+                // Calculates the volume of concrete required
+                volume = length * width * DRIVEWAY_DEPTH;
+
+                // Calculates the amount of cement needed (kg)
+                weight = volume / CONCRETE_PER_KG;
+
+                // Calculates the number of bags required
+                bags = (int)Math.Ceiling(weight / BAG_WEIGHT);
+
+                // Calculates the total cost of the cement
+                cost = bags * BAG_COST;
+
+                // Displays the results
+                textBoxVolume.Text = volume.ToString("n3");
+                textBoxWeight.Text = weight.ToString("n3");
+                textBoxBags.Text = bags.ToString();
+                textBoxCost.Text = cost.ToString("c");
+
+            }
+            catch
+            {
+                // Displays error message if the values are not valid
+                MessageBox.Show("Please enter valid values");
+
+                // Clears all the text boxes
+                textBoxWidth.Clear();
+                textBoxLength.Clear();
+                textBoxCost.Clear();
+                textBoxVolume.Clear();
+                textBoxWeight.Clear();
+                textBoxBags.Clear();
+
+                // Sets focus to first text box
+                textBoxWidth.Focus();
+            }
+
+        }
+
+        /// <summary>
+        /// The method clears all the text boxes and sets the focus to the first textbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            // Clears the text boxes
+            textBoxWidth.Clear();
+            textBoxLength.Clear();
+            textBoxCost.Clear();
+            textBoxVolume.Clear();
+            textBoxWeight.Clear();
+            textBoxBags.Clear();
+
+            // Sets focus to first text box
+            textBoxWidth.Focus();
+
+        }
+
+        /// <summary>
+        /// This method displays information on how to use the
+        /// concrete caclulator
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            // Displays help information
+            MessageBox.Show(
+                "Concrete Calculator Help\n\n" +
+                "1. Enter the length (0–100 meters).\n" +
+                "2. Enter the width (0–100 meters).\n" +
+                "3. Click 'Calculate'.\n\n" +
+                "Use 'Clear' to reset.\n" +
+                "Click 'Exit' to close the program.",
+                "Help",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        /// <summary>
+        /// The method exits the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            // Exits the application
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxWeight_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxLength_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
